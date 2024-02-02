803af54c5556a9a28e9523ed60004bf64a32510c
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-02-02 15:35:39 +0100

    more structuring

--- src/DGFeeManager.sol
+++ src/DGFeeManager.sol
@@ -8,6 +8,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 import {DGDataTypes} from "src/libraries/DGDataTypes.sol";
+import {DGEvents} from "src/libraries/DGEvents.sol";
 
 contract DGFeeManager is Ownable {
     /// @dev basis points denominator used for percentage calculation
@@ -16,9 +17,19 @@ contract DGFeeManager is Ownable {
     /// @dev Event period, the minimum time between each claim
     uint256 public constant EVENT_PERIOD = 30 days;
 
+    /// @dev store bankroll status
     mapping(address bankroll => bool isApproved) public bankrollStatus;
+
+    /// @dev store the fee schema for a given bankroll
+    mapping(address bankroll => DGDataTypes.Fee fee) public bankrollFees;
 
     function approveBankroll(address _bankroll) external onlyOwner {
         bankrollStatus[_bankroll] = true;
+    }
+
+    function blockBankroll(address _bankroll) external onlyOwener {
+        bankrollStatus[_bankroll] = false;
+
+        bankrollFees[_bankroll] = DGDataTypes.Fee(0, 0, 0, 0);
     }
 }
--- src/libraries/DGEvents.sol
+++ src/libraries/DGEvents.sol
@@ -4,8 +4,24 @@ pragma solidity ^0.8.18;
 library DGEvents {
     event FundsDeposited(address lp, uint256 amount);
     event FundsWithdrawn(address lp, uint256 amount);
+    
+    /// @dev Event emitted when debit function is called
     event Debit(address manager, address player, uint256 amount);
+    
+    /// @dev Event emitted when Credit function is called
     event Credit(address manager, uint256 amount);
+    
     event ProfitClaimed(address manager, uint256 amount);
+    
+    /// @dev Event emitted when the bankroll is emptied or reached max risk
     event BankrollSwept(address player, uint256 amount);
+    
+    /// @dev Event emitted when the stakeholder fee are updated
+    event FeeUpdated(
+        address bankroll, 
+        uint64 deGamingFee,
+        uint64 bankRollFee, 
+        uint64 gameProviderFee, 
+        uint64 managerFee
+    );
 }
