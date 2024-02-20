89c780b2150dab16ede73d5a1a3a192aa94a9472
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-02-20 10:53:20 +0100

    initialization  functions added for dgbankroll factory

--- src/DGBankrollFactory.sol
+++ src/DGBankrollFactory.sol
@@ -16,5 +16,73 @@ import {Bankroll} from "src/Bankroll.sol";
  */
 
 contract DGBankrollFactory is AccessControl {
+    //     _____ __        __
+    //    / ___// /_____ _/ /____  _____
+    //    \__ \/ __/ __ `/ __/ _ \/ ___/
+    //   ___/ / /_/ /_/ / /_/  __(__  )
+    //  /____/\__/\__,_/\__/\___/____/
+
+    /// @dev number of collection created by this factory
+    uint256 public bankrollCount;
+
+    /// @dev Array of all Bankrolls created by this factory
+    address[] public bankrolls;
+
+    /// @dev Standard DeGaming Bankroll contract implementation address
+    address public bankrollImpl;
+
+    /// @dev DeGaming Bankroll Manager Contract address
+    address dgBankrollManager;
+
+    /// @dev DeGaming admin account
+    address public dgAdmin;
+
+    /// @dev Storage gap used for future upgrades (30 * 32 bytes)
+    uint256[30] __gap;
+
+    //    ______                 __                  __
+    //   / ____/___  ____  _____/ /________  _______/ /_____  _____
+    //  / /   / __ \/ __ \/ ___/ __/ ___/ / / / ___/ __/ __ \/ ___/
+    // / /___/ /_/ / / / (__  ) /_/ /  / /_/ / /__/ /_/ /_/ / /
+    // \____/\____/_/ /_/____/\__/_/   \__,_/\___/\__/\____/_/
+
+    /**
+     * @notice
+     *  Contract Constructor
+     */
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        address _bankrollImpl,
+        address _dgBankrollManager,
+        address _dgAdmin
+    ) external initializer {
+        bankrollImpl = _bankrollImpl;
+        dgBankrollManager = _dgBankrollManager;
+        dgAdmin = _dgAdmin;
+
+        __AccessControl_init();
+    }
+
+    //   _    ___                 ______                 __  _
+    //  | |  / (_)__ _      __   / ____/_  ______  _____/ /_(_)___  ____  _____
+    //  | | / / / _ \ | /| / /  / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+    //  | |/ / /  __/ |/ |/ /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
+    //  |___/_/\___/|__/|__/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+
+    /**
+     * @notice
+     *  Predict the new Bankroll contract address
+     *
+     * @param _salt salt used for the deterministic deployment
+     *
+     * @return _predicted predicted address for the given `_salt`
+     */
+    function predictBankrollAddress(bytes32 _salt) external view returns (address _predicted) {
+        _predicted = Clones.predictDeterministicAddress(bankrollImpl, _salt, address(this));
+    }
 
 }
