a72cc919a36c3b2d61e9543388d7180d956e6ff3
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-03-01 14:18:15 +0100

    extended deployment script

--- script/deploy-Bankroll.s.sol
+++ script/deploy-Bankroll.s.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import {Bankroll} from "src/Bankroll.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 
 contract DeployBankroll is Script {
+    using SafeERC20 for IERC20; 
     function run() public {
         // Private keys
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
@@ -35,10 +38,14 @@ contract DeployBankroll is Script {
 
         // Set bankroll max allowance
         vm.startBroadcast(managerPrivateKey);
-        IERC20(token).approve(
+        // IERC20(token).approve(
+            // address(bankroll),
+            // 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        // ); //max int value
+        IERC20(token).forceApprove(
             address(bankroll),
             0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-        ); //max int value
+        );
         vm.stopBroadcast();
     }
 }
--- script/deploy-BankrollAndToken.s.sol
+++ script/deploy-BankrollAndToken.s.sol
@@ -7,16 +7,20 @@ import {MockToken} from "test/mock/MockToken.sol";
 
 contract DeployBankroll is Script {
     function run() public {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address admin = vm.envAddress("ADMIN_ADDRESS");
-        address manager = vm.envAddress("MANAGER_ADDRESS");
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        uint256 adminPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
+        uint256 managerPrivateKey = vm.envUint("MANAGER_PRIVATE_KEY");
+
+        address admin = vm.addr(adminPrivateKey);
+        address manager = vm.addr(managerPrivateKey);
         uint256 maxRisk = 10_000;
         vm.startBroadcast(deployerPrivateKey);
 
         // deploy mock token
         MockToken token = new MockToken("Tether", "mUSDT");
         Bankroll bankroll = new Bankroll(admin, address(token), maxRisk);
-        bankroll.setManager(manager, true);
+        //bankroll.setManager(manager, true);
 
         vm.stopBroadcast();
     }
