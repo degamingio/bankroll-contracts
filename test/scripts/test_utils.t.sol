8c73b1aa14b5a431c6a0fd4059edf16e96d77f6e
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-03-05 16:22:42 +0100

    setup for utils done

--- test/scripts/test_utils.t.sol
+++ test/scripts/test_utils.t.sol
@@ -21,3 +21,49 @@ import {CreateBankroll} from "script/utils/DGBankrollFactory/create-bankroll.s.s
 
 /* MockToken Utils */
 import {MintTokens} from "script/utils/MockToken/mint-token.s.sol";
+
+contract UtilTest is Test {
+    Credit creditCall;
+    Debit debitCall;
+    DepositFunds depositFundsCall;
+    WithdrawFunds withdrawFundsCall;
+
+    AddBankroll addBankrollCall;
+    AddOperator addOperatorCall;
+    BlockBankroll blockBankrollCall;
+    BlockOperator blockOperaotrCall;
+    ClaimProfit claimProfitCall;
+
+    CreateBankroll createBankrollCall;
+
+    MintTokens mintTokensCall;
+
+    function setUp() public {
+        creditCall = new Credit();
+        debitCall = new Debit();
+        depositFundsCall = new DepositFunds();
+        withdrawFundsCall = new WithdrawFunds();
+
+        addBankrollCall = new AddBankroll();
+        addOperatorCall = new AddOperator();
+        blockBankrollCall = new BlockBankroll();
+        blockOperaotrCall = new BlockOperator();
+        claimProfitCall = new ClaimProfit();
+
+        createBankrollCall = new CreateBankroll();
+
+        mintTokensCall = new MintTokens();
+    }
+
+    // function test_creditCall() public{
+        // creditCall.run();
+    // }
+
+    // function test_debitCall() public {
+        // debitCall.run();
+    // }
+
+    function test_mintTokensCall() public {
+        mintTokensCall.run();
+    }
+}
