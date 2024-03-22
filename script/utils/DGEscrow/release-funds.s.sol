38b5daea36521ac5f9a344137dd9f0fef53f7d69
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-03-22 12:57:41 +0100

    everything tested

--- script/utils/DGEscrow/release-funds.s.sol
+++ script/utils/DGEscrow/release-funds.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 //import {DGBankrollManager} from "src/DGBankrollManager.sol";
 import {DGEscrow} from "src/DGEscrow.sol";
+import {DGDataTypes} from "src/libraries/DGDataTypes.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -27,7 +28,19 @@ contract ClaimProfit is Script {
         vm.startBroadcast(adminPrivateKey);
 
         // PASTE ID HERE
-        //escrow.releaseFunds(vm.parseBytes(vm.readFile(ID_PATH)));
+        //escrow.releaseFunds(vm.parseBytes(vm.readFile(ID_PATH))); 
+
+        DGDataTypes.EscrowEntry memory entry = DGDataTypes.EscrowEntry(
+            0x242f2d72f6B9F534B6182d911e719b7be7a81861,
+            0x312BA46064e1fe5904362BfB0C52c38F20d3Ef44,
+            0xDef28f7d7F3700e30F403B6350Fb54a358469874,
+            0xc6ADeA8722C2DB1EFF810f429C3C09BA00E1F25C,
+            1711105908
+        );
+
+        bytes memory id = abi.encode(entry);
+
+        escrow.releaseFunds(id);
 
         vm.stopBroadcast();
     }
--- script/utils/DGEscrow/revert-funds.s.sol
+++ script/utils/DGEscrow/revert-funds.s.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 //import {DGBankrollManager} from "src/DGBankrollManager.sol";
 import {DGEscrow} from "src/DGEscrow.sol";
-
+import {DGDataTypes} from "src/libraries/DGDataTypes.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ClaimProfit is Script {
@@ -26,8 +26,18 @@ contract ClaimProfit is Script {
     function run() public {
         vm.startBroadcast(adminPrivateKey);
 
+        DGDataTypes.EscrowEntry memory entry = DGDataTypes.EscrowEntry(
+            0x242f2d72f6B9F534B6182d911e719b7be7a81861,
+            0x312BA46064e1fe5904362BfB0C52c38F20d3Ef44,
+            0xDef28f7d7F3700e30F403B6350Fb54a358469874,
+            0xc6ADeA8722C2DB1EFF810f429C3C09BA00E1F25C,
+            1711104158
+        );
+
+        bytes memory id = abi.encode(entry);
+
         // PASTE ID HERE
-        // escrow.revertFunds(vm.parseBytes(vm.readFile(ID_PATH)));
+        escrow.revertFunds(id);
 
         vm.stopBroadcast();
     }
